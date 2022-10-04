22ca87f582549dec0f345a28750c4638e509a443
Charbel Sarkis <charbelsako@gmail.com>
2022-10-04 22:52:55 +0300

    Fixes types in deleteFile api

--- pages/api/deleteFile.ts
+++ pages/api/deleteFile.ts
@@ -6,10 +6,19 @@ interface Data {
   deletedFile: File;
 }
 
+interface Error {
+  message: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data | Error>
 ): Promise<void> {
-  const deletedFile: File = await sanityClient.delete(req.query.id);
+  if (req.query.id !== null) {
+    res.status(500).json({ message: 'No ID was passed' });
+    return;
+  }
+  const id: string = Array.isArray(req.query.id) ? req.query.id[0] : req.query.id;
+  const deletedFile: File = await sanityClient.delete(id);
   res.status(200).json({ deletedFile });
 }
